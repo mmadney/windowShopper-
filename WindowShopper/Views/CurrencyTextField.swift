4b67b1f25bf8fec9a54ceafac254b7e37ec46ce2
Mohamed <mohamedmadney1@gmail.com>
2020-02-27 02:16:05 -0800

    Add Currency Lbl

--- WindowShopper/Views/CurrencyTextField.swift
+++ WindowShopper/Views/CurrencyTextField.swift
@@ -10,7 +10,27 @@ import UIKit
 
 
 @IBDesignable
+
 class CurrencyTextField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        drawCurrencyLbl(size: size)
+    }
+    
+    func drawCurrencyLbl(size : CGFloat){
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2 , width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.5629340056, green: 0.5629340056, blue: 0.5629340056, alpha: 1)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
     override func prepareForInterfaceBuilder() {
         CustoimzeView()
     }
